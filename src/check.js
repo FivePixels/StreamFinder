034720cdad9e7344db4177a6fef89c6b29a5829a
o5pxels <o5pxels@gmail.com>
2021-04-10 14:20:15 -0500

    check.js incomplete implementation, move modules to src, aexport scrape function

--- src/check.js
+++ src/check.js
@@ -1,4 +1,18 @@
-const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
 // This is for passing the parameters of the search to check and see if it already exists in the database
 // if it does exist, we're gonna call another function in another file.
+
+const scraper = require('./scraper/scrape')
+
+exports.performCheck = async function performCheck(query, type) {
+    // if (!exist in database) {
+        await scraper.performSearch(query, type);
+    // }
+    // (if it needed to be scraped, it now is, and its stored. next, we perform the database search for the newly saved entry)
+    performDatabaseSearch(query, type);
+}
+
+async function performDatabaseSearch(query, type) {
+    // do database search
+}
