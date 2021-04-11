1a75774bd8b43d76a1cc9fb07817f39df849001b
realS1ant <trb.9900@gmail.com>
2021-04-11 02:51:15 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 const { performCheck } = require('./check');
 require('dotenv').config();
 
 app.use(express.json());
+app.use(cors());
 if (process.env.ENV == 'dev') app.use(morgan('dev'));
 
 app.get('/api/performCheck', async (req, res) => {
